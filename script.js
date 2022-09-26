c9965b58ecfb60543e20d30e14b1d9fdf3c8b925
Borislava Yordanova <borislava_yordanova@abv.bg>
2022-09-27 00:07:28 +0100

    Final Project

--- script.js
+++ script.js
@@ -11,26 +11,45 @@ function fillValues()
 
 function compute()
 {
-    var principal = document.getElementById("principal").value; 
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
+    if(isInputValid())
+    {
+        var principal = document.getElementById("principal").value;
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate /100;
+        var year = new Date().getFullYear()+parseInt(years);
 
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+        document.getElementById("result").innerHTML=
+        `If you deposit ${setFontColorToYellow(principal)} at an interest rate of `
+        +`${setFontColorToYellow(rate)}% You will receive an amount of `
+        +`${setFontColorToYellow(interest)} in the year `
+        +`${setFontColorToYellow(year)}`;
+    }
 }
 
-function updateRate() 
+function setFontColorToYellow(element)
+{
+    return `<span style="color:#FFC300">${element}</span>\<br\>`
+}
+
+function updateRate()
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=`${rateval}%`;
 }
 
-function validateInput()
+function isInputValid()
 {
-    var principal = document.getElementById("principal").value;
-    if(principal<=0)
-    {        
-        window.alert("Enter a positive number");
+    var isValid = false;
+    var principal = document.getElementById("principal");
+    if(principal.value <= 0)
+    {
+        confirm("Enter a positive number");
+        principal.focus();
     }
+    else
+    {
+        isValid = true;
+    }
+    return isValid;
 }
