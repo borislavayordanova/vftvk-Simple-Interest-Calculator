c9d6a42015fe199a76c020f3bff951cd56de2e8e
Borislava Yordanova <borislava_yordanova@abv.bg>
2022-09-26 23:12:28 +0100

    Final project

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
+function fillValues()
+{
+    var select = document.getElementById("years");
+    for(var i = 1; i <= 20; i++) {
+        select.options[select.options.length] = new Option(`${i}`, `${i}`);
+    };
+
+    var defaultRate = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = `${defaultRate}%`;
+}
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value; 
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
 }
-        
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+function validateInput()
+{
+    var principal = document.getElementById("principal").value;
+    if(principal<=0)
+    {        
+        window.alert("Enter a positive number");
+    }
+}
